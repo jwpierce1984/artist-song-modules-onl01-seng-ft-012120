315e2509881b37773b183b62902c9ab5822f57b5
Justin Pierce <jwpierce1984@gmail.com>
2020-02-05 20:48:32 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,12 +1,14 @@
 module Memorable
-  module Class
-  def self.reset_all
-    self.all.clear
-  end
+  module ClassMethods
+    def self.reset_all
+      self.all.clear
+    end
 
-  def self.count
-    self.all.count
+    def self.count
+      self.all.count
+    end
   end
+  
 
   
 end
