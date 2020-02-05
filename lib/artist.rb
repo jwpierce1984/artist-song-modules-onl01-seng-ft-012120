e5f9a6e23f824f2a20be46161a7cc4361fbf862a
Justin Pierce <jwpierce1984@gmail.com>
2020-02-05 20:32:19 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -18,6 +18,15 @@ class Artist
   def self.all
     @@artists
   end
+  
+    def self.reset_all
+    self.all.clear
+  end
+
+  def self.count
+    self.all.count
+  end
+
 
   def add_song(song)
     @songs << song
