6453de1c2685e606dfaa21cdc007d58c6a1958bc
Justin Pierce <jwpierce1984@gmail.com>
2020-02-05 21:11:50 +0000

    Automatically backed up by Learn

--- lib/artist.rb
+++ lib/artist.rb
@@ -5,7 +5,7 @@ class Artist
   extend Memorable::ClassMethods
   extend Findable::ClassMethods
   include Memorable::InstanceMethods
-  include Paramble::InstanceMethods
+  include Paramable::InstanceMethods
   
   attr_accessor :name
   attr_reader :songs
